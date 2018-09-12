ad043375c64200a15c7823c79b910b4094759f0f
iEZcheez <kevtran.net@gmail.com>
2018-09-12 14:07:15 -0400

    Update Lab2.js

--- Lab2.js
+++ Lab2.js
@@ -27,3 +27,16 @@ class Student extends Person {
         console.log("I am a student named ", name);
     }
 }
+
+//From example
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+console.log('age: ' + getAge("1980/08/10"));
