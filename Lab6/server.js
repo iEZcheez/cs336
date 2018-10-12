269113b961c77981b4726b12c6e423e7f33c4903
EZcheez <kevtran.net@gmail.com>
2018-10-11 22:21:00 -0400

    Lab06

--- Lab6/server.js
+++ Lab6/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+// body parser
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
 
@@ -7,11 +9,16 @@ app.use(express.static('files'))
 app.use('/', express.static('public'))
 
 // body parser
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send('Lab06: Please add index.html for main page'))
+
+// Exercise 6.2 - POST - Receive form and redirect to /forms
+app.post('/forms', function (req, res) {
+    // res.redirect('/forms')
+    res.json(req.body)
+})
 
 // Requests using GET should only retrieve data.
 app.get('/request', function (req, res) {
@@ -25,12 +32,14 @@ app.head('/request', function (req, res) {
 
 // The POST method is used to submit an entity to the specified resource, often causing a change in state or side effects on the server.
 app.post('/request', function (req, res) {
-    res.send('Got a POST request: ' + req.body.arg)
+    // res.send('Got a POST request')
+    res.json(res.body)
 })
 
 // The PUT method replaces all current representations of the target resource with the request payload.
 app.put('/request', function (req, res) {
-    res.send('Got a PUT request: ' + req.body.arg)
+    // res.send('Got a PUT request')
+    res.json(res.body)
 })
 
 // The DELETE method deletes the specified resource.
