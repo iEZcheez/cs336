b0acc5060b7f5efb613cd19b12778afc83cf26ac
iEZcheez <kevtran.net@gmail.com>
2018-11-14 13:37:24 -0500

    Update webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -41,15 +41,7 @@ module.exports = {
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin(),
         new ExtractTextPlugin("[name]-[hash].css")
-    ],
-    devServer: {
-        port: 3001,
-        proxy: { '/api/*': 'http://localhost:3000' },
-        colors: true,
-        historyApiFallback: true,
-        inline: true,
-        hot: true
-    }
+    ]
 };
 
 //     This specifies the application’s source code modules (i.e., entry),
