d722639374753f7267f2e8f776e6e630dd118825
iEZcheez <kevtran.net@gmail.com>
2018-11-26 17:40:24 -0500

    Lab13

--- Lab13/app/scripts/index.js
+++ Lab13/app/scripts/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Lab13
+import {Router, Route, browserHistory} from 'react-router'
+
 import CommentBox from './CommentBox'
 
 import '../css/base.css';
@@ -11,6 +14,11 @@ ReactDOM.render(
     //React classes begin with uppercase letters
     //Refresh the comments every 2 seconds
     //"/api/comments" accesses comments.json in the directory
-    <CommentBox url="/api/comments" pollInterval={2000} />,
+    (
+        <Router history={browserHistory}>
+        <Route path="/" component={CommentBox}/}
+        <Route path="/:id" component={CommentEdit}/}
+    </Router>
+    ),
     document.getElementById('content')
 );
