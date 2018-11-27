b393f040022a1b9d0937ca8619336624793aae9c
iEZcheez <kevtran.net@gmail.com>
2018-11-26 23:30:19 -0500

    Lab13

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Lab13
-import {Router, Route, browserHistory} from 'react-router'
+import {Router, Route, browserHistory} from 'react-router';
 
-import CommentBox from './CommentBox'
+import CommentBox from './commentBox';
+import CommentEdit from './commentEdit';
 
 import '../css/base.css';
 
-// PART FINAL: RENDER
-// React components implement a render() method that
-// takes input data and returns what to display
-ReactDOM.render(
-    //React classes begin with uppercase letters
-    //Refresh the comments every 2 seconds
-    //"/api/comments" accesses comments.json in the directory
-    (
+ReactDOM.render((
         <Router history={browserHistory}>
-        <Route path="/" component={CommentBox}/}
-        <Route path="/:id" component={CommentEdit}/}
-    </Router>
-    ),
-    document.getElementById('content')
+            <Route path="/" component={CommentBox}/>
+            <Route path="/:id" component={CommentEdit}/>
+        </Router>
+    ), document.getElementById('content')
 );
